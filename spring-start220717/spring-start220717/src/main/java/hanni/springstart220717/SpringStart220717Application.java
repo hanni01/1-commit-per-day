78907308c0eebe189c33c8568d6facd6cf93df34
hanni01 <he3080@gmail.com>
2022-07-17 23:40:25 +0900

    test

--- spring-start220717/spring-start220717/src/main/java/hanni/springstart220717/SpringStart220717Application.java
+++ spring-start220717/spring-start220717/src/main/java/hanni/springstart220717/SpringStart220717Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringStart220717Application {
-
+	//start spring !!
 	public static void main(String[] args) {
 		SpringApplication.run(SpringStart220717Application.class, args);
 	}
