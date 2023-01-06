c68258e2441f2dc8a074648c65c414d01980d590
hanni01 <he3080@gmail.com>
2023-01-06 23:39:31 +0900

    programmers lv1

---
+++ 개인정보수집유효기간.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+class 개인정보수집유효기간 {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        int[] answer = {};
+        String[][] tType = new String[terms.length][2];
+
+        for(int i = 0;i< terms.length;i++)
+        {
+            tType[i] = terms[i].split(" ");
+        }
+        return answer;
+    }
+}
