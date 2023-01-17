b166e92f92edde973ea99eb0b807667c148c81eb
정하은 <99061922+hanni01@users.noreply.github.com>
2023-01-17 22:19:16 +0900

    Delete nestjs-board-app directory

--- nestjs-board-app/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { BoardsModule } from './boards/boards.module';
-import { typeORMConfig } from './config/typeorm.config';
-
-@Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
-})
-export class AppModule {}
